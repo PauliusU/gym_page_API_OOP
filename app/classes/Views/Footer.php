a31823f6979fdda12bea4695e2f27d6087907c2d
PauliusU <48020370+PauliusU@users.noreply.github.com>
2020-12-21 22:14:57 +0200

    Update navigation to display links in two different sections (left and right), also to indicate link of active page

--- app/classes/Views/Footer.php
+++ app/classes/Views/Footer.php
@@ -14,7 +14,7 @@ class Footer extends View
     public function generate()
     {
         $current_year = date('Y');
-        return "Copyright © $current_year Paulius Uosis, all rights reserved.";
+        return "© $current_year. Paulius Uosis, all rights reserved.";
     }
 
     public function render($template_path = ROOT . '/app/templates/footer.php')
